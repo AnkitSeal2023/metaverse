084ad44f73b6200af597f726ce5e52acbcc8549b
Ankit Seal <ankitseal2021@gmail.com>
2024-11-08 10:16:02 +0530

    empty commit

--- frontend/src/containers/Game.tsx
+++ frontend/src/containers/Game.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../index.css'
-import { Game } from '../components/game'
+// import { Game } from '../components/game'
+import { Game } from '../components/test'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Game/>
